511922cf3c7eb4d5ec38f202617ac3a463351dd1
hacadena28 <hacadena@unicesar.edu.cong>
2022-11-11 14:17:57 -0500

    comit v1 new api

---
+++ NetCoreAPIMySQL.Model/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoreAPIMySQL.Model
+{
+    public class Usuario
+    {
+        private int idUsuario;
+        private string correo;
+        private string contrasena;
+        private string tipoUsuario;
+    }
+}
