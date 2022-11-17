175dd8789235a7483524057f9016aa2c25f02a92
hacadena28 <hacadena@unicesar.edu.cong>
2022-11-17 16:26:09 -0500

    api v2.1 final

--- NetCoreAPIMySQL/Startup.cs
+++ NetCoreAPIMySQL/Startup.cs
@@ -40,7 +40,7 @@ namespace NetCoreAPIMySQL
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "NetCoreAPIMySQL", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Consultorio Juridivo", Version = "v2" });
             });
 
 
@@ -53,7 +53,7 @@ namespace NetCoreAPIMySQL
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NetCoreAPIMySQL v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Consultorio Juridivo"));
             }
 
             app.UseHttpsRedirection();
